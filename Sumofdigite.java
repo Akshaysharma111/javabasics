d49e22558ba54f98c9faa7e1437bdb80d3611671
Akshaysharma111 <118056063+Akshaysharma111@users.noreply.github.com>
2022-11-30 18:18:55 +0530

    Add files via upload

---
+++ Sumofdigite.java
@@ -0,0 +1,13 @@
+
+public class Sumofdigite {
+	    public static int operation(int number) {
+	        if(number <= 0)
+	            return 0;
+	        return number % 10 + operation(number/10);
+	    }
+	    public static void main(String[] args) {
+	        int num = 123456;
+	        System.out.println("The Sum of Digit of Number "+num+" is : "+operation(num));
+	    }
+	}
+
